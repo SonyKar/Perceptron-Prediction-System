e7daf3c2a9a49093ba6144db47e284228edd7516
SonyKar <sa.caraganciu@gmail.com>
2023-01-10 13:16:16 +0200

    Statistics  - Added and displayed predictor statistics in form

--- Form1.cs
+++ Form1.cs
@@ -13,12 +13,29 @@ namespace PerceptronPredictionSystem
 
 		private void startButton_Click(object sender, EventArgs e)
 		{
-			if (data != null)
+			if (nrPerceptronTextBox.Text == "" || nrHrBitsTextBox.Text == "")
+			{
+				MessageBox.Show("Select Nr of perceptrons and Nr of bits in HR");
+			}
+			else if (data != null)
 			{
 				Predictor predictor = new Predictor(Int32.Parse(nrPerceptronTextBox.Text),
 												Int32.Parse(nrHrBitsTextBox.Text),
 												data);
 				predictor.Train();
+
+				double accuracyTaken = predictor.totalJumpsTakenPredicted * 1.0 / predictor.totalJumpsTaken;
+				double accuracyNotTaken = predictor.totalJumpsNotTakenPredicted * 1.0 / predictor.totalJumpsNotTaken;
+				double accuracy = (predictor.totalJumpsTakenPredicted + predictor.totalJumpsNotTakenPredicted) * 1.0 / predictor.totalJumps;
+
+				totalJumpsLabel.Text = "Total Jumps: " + predictor.totalJumps;
+				takenTotalLabel.Text = "Taken total: " + predictor.totalJumpsTaken;
+				takenPredictedLabel.Text = "Taken predicted: " + predictor.totalJumpsTakenPredicted;
+				notTakenTotalLabel.Text = "Not-Taken total: " + predictor.totalJumpsNotTaken;
+				notTakenPredictedLabel.Text = "Not-Taken predicted: " + predictor.totalJumpsNotTakenPredicted;
+				accuracyLabel.Text = "Accuracy: " + (accuracy * 100.0) + "%";
+				accuracyTakenLabel.Text = "Accuracy taken: " + (accuracyTaken * 100.0) + "%";
+				accuracyNotTakenLabel.Text = "Accuracy not-taken: " + (accuracyNotTaken * 100.0) + "%";
 			}
 			else
 			{
@@ -33,9 +50,10 @@ namespace PerceptronPredictionSystem
 
 			if (result == DialogResult.OK) // Test result.
 			{
-				startButton.Enabled = true;
-
 				string file = openFileDialog1.FileName;
+				startButton.Enabled = true;
+				filenameLabel.Text = file;
+
 				data = Data.GetData(file);
 			}
 		}
--- Predictor.cs
+++ Predictor.cs
@@ -8,11 +8,18 @@ namespace PerceptronPredictionSystem
         private readonly HRg hrg;
         private readonly Data[] data;
 
+        public int totalJumps { get; private set; }
+        public int totalJumpsTaken { get; private set; }
+        public int totalJumpsTakenPredicted { get; private set; }
+		public int totalJumpsNotTaken { get; private set; }
+		public int totalJumpsNotTakenPredicted { get; private set; }
+
 		public Predictor(int perceptronNumber, int hrgSize, Data[] data)
         {
             perceptrons = new Perceptron[perceptronNumber];
             hrg = new HRg(hrgSize);
             this.data = data;
+            totalJumps = data.Length;
 
             for(int i = 0; i < perceptrons.Length; i++)
             {
@@ -27,9 +34,18 @@ namespace PerceptronPredictionSystem
 				int index = dataPiece.PC % perceptrons.Length;
 
 				bool predictedValue = perceptrons[index].Calculate(hrg);
+
+                if (dataPiece.isTaken) totalJumpsTaken++;
+				else totalJumpsNotTaken++;
+
 				if (predictedValue != dataPiece.isTaken) // not equal real value
 				{
 					perceptrons[index].AdjustWeights(dataPiece.isTaken); // real value
+				}
+                else
+                {
+					if (predictedValue) totalJumpsTakenPredicted++;
+					else totalJumpsNotTakenPredicted++;
 				}
 				hrg.AddValue(dataPiece.isTaken);
 			}
