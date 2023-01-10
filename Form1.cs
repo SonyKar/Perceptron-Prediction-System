5e8da82c20f8e24580399a0e910ade78feb2cfe8
SonyKar <sa.caraganciu@gmail.com>
2023-01-10 12:26:05 +0200

    File read  - Added file read and data storage Data class  - Added browse file functionality

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,11 @@
+using PerceptronPredictionSystem.Model;
+
 namespace PerceptronPredictionSystem
 {
 	public partial class Form : System.Windows.Forms.Form
 	{
+		private Data[]? data;
+
 		public Form()
 		{
 			InitializeComponent();
@@ -9,12 +13,31 @@ namespace PerceptronPredictionSystem
 
 		private void startButton_Click(object sender, EventArgs e)
 		{
-			
+			if (data != null)
+			{
+				//Predictor predictor = new Predictor(Int32.Parse(nrPerceptronTextBox.Text),
+				//								Int32.Parse(nrHrBitsTextBox.Text),
+				//								data);
+				//predictor.Train();
+			}
+			else
+			{
+				MessageBox.Show("The file doesn't contain data");
+			}
 		}
 
 		private void chooseFileButton_Click(object sender, EventArgs e)
 		{
-			
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+			DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+			if (result == DialogResult.OK) // Test result.
+			{
+				startButton.Enabled = true;
+
+				string file = openFileDialog1.FileName;
+				data = Data.GetData(file);
+			}
 		}
 	}
 }
